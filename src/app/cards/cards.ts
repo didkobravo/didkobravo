56b2050eb5a02823b8d85b5146894022348d951f
Deyan Angelov <didkobravo@gmail.com>
2025-10-20 20:19:39 +0300

    animate hero

---
+++ src/app/cards/cards.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './cards.html',
+  styleUrl: './cards.scss'
+})
+export class Cards {
+
+}
