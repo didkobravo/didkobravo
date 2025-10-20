8a559cfed4e402e461e36c7e11bbcbbf6e01e971
Deyan Angelov <didkobravo@gmail.com>
2025-10-20 23:18:37 +0300

    adding cards

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { Hero } from '../hero/hero';
+import { Cards } from '../cards/cards';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero],
+  imports: [Hero, Cards],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
